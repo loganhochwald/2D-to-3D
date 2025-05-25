50088c75042defdd115d4ae6f1b9b2ecf9bebfef
Logan Hochwald <loganhochwald@gmail.com>
2025-05-25 18:15:58 -0500

    feat: update editor machine to use typescript and contexts

--- src/machines/editorMachine.ts
+++ src/machines/editorMachine.ts
@@ -1,18 +1,55 @@
 import { createMachine } from 'xstate';
+import type { Shape } from '../types';
 
-export const editorMachine = createMachine({
-  id: 'editor',
-  initial: 'main',
-  states: {
-    main: {
-      on: {
-        SELECT_SHAPE: 'editing',
-      },
+interface Context {
+  shapes: Shape[];
+  selectedShape?: Shape;
+}
+
+export const editorMachine = createMachine(
+  {
+    types: {
+      context: {} as Context,
+      events: {} as
+        | { type: 'SELECT_SHAPE'; shape: Shape }
+        | { type: 'DESELECT_SHAPE' },
     },
-    editing: {
-      on: {
-        DESELECT_SHAPE: 'main',
+
+    id: 'editor',
+    initial: 'main',
+    context: {
+      shapes: [],
+    },
+
+    states: {
+      main: {
+        on: {
+          SELECT_SHAPE: {
+            target: 'editing',
+            actions: 'selectShape',
+          },
+        },
+      },
+      editing: {
+        on: {
+          DESELECT_SHAPE: {
+            target: 'main',
+            actions: 'deselectShape',
+          },
+        },
       },
     },
   },
-});
+  {
+    actions: {
+      selectShape: ({ context, event }) => {
+        if (event.type === 'SELECT_SHAPE') {
+          context.selectedShape = event.shape;
+        }
+      },
+      deselectShape: ({ context }) => {
+        context.selectedShape = undefined;
+      },
+    },
+  },
+);
