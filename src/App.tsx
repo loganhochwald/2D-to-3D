dc7f9160e2aa277aeb3bb9fba76e3d2871b5b17b
Logan Hochwald <loganhochwald@gmail.com>
2025-05-24 22:44:49 -0500

    style: make code editor bottom fourth of page

--- src/App.tsx
+++ src/App.tsx
@@ -9,10 +9,10 @@ function App() {
 
   return (
     <div className="flex flex-col h-screen bg-gray-900">
-      <div className="h-1/2">
+      <div className="h-3/4">
         <Scene shapes={shapes} />
       </div>
-      <div className="h-1/2">
+      <div className="h-1/4">
         <Editor initialDoc={code} onChange={(val) => setCode(val)} />
       </div>
     </div>
