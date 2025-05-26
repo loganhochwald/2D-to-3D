792f498910f6c56b0db3ccf55c60f44e2bb35e8e
Logan Hochwald <loganhochwald@gmail.com>
2025-05-26 18:43:26 -0500

    feat: add linear velocity to question mark and style with edges

--- src/components/DSLGuidePanel.tsx
+++ src/components/DSLGuidePanel.tsx
@@ -1,4 +1,4 @@
-import { Text3D } from '@react-three/drei';
+import { Text3D, Edges, useCursor } from '@react-three/drei';
 import { useState } from 'react';
 import { RigidBody } from '@react-three/rapier';
 
@@ -8,18 +8,26 @@ interface DSLGuidePanelProps {
 
 const DSLGuidePanel: React.FC<DSLGuidePanelProps> = ({ send }) => {
   const [hovered, setHovered] = useState(false);
+  useCursor(hovered);
 
   return (
-    <RigidBody type="dynamic" colliders="hull">
+    <RigidBody
+      type="dynamic"
+      linearVelocity={[0.05, 0, 0]}
+      linearDamping={0}
+      angularDamping={0}
+      canSleep={false}
+    >
       <group
-        position={[-2, 2, 0]}
+        position={[-2, 3, 0]}
         onClick={() => send({ type: 'TOGGLE_GUIDE' })}
         onPointerOver={() => setHovered(true)}
         onPointerOut={() => setHovered(false)}
       >
-        <Text3D height={0.2} size={0.8} font="public\Inter_Bold.json">
+        <Text3D height={0.1} size={0.4} font="public/Inter_Bold.json">
           ?
           <meshStandardMaterial color={hovered ? '#ffaaaa' : 'red'} />
+          <Edges scale={1.01} threshold={15} color="black" />
         </Text3D>
       </group>
     </RigidBody>
