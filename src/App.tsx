4768e777434936ba01976a7b905ff4d088cd4169
Logan Hochwald <loganhochwald@gmail.com>
2025-05-26 14:45:14 -0500

    feat: move all panels to scene and remove dsl tooltip for now

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,22 @@
 import { type FC } from 'react';
-import Editor from './components/Editor';
 import SceneRenderer from './components/SceneRenderer';
-import DSLTooltip from './components/DSLTooltip';
 import { useMachine } from '@xstate/react';
 import { editorMachine } from './machines/editorMachine';
-import EditingPanel from './components/EditingPanel';
 
 const App: FC = () => {
   const [state, send] = useMachine(editorMachine);
   const { code, shapes, selectedShape } = state.context;
 
   return (
-    <div className="h-dvh bg-black p-4 sm:px-8 flex flex-col">
-      <DSLTooltip />
-      <div className="flex-[3]">
-        <SceneRenderer
-          shapes={shapes}
-          send={send}
-          selectedShape={selectedShape}
-        />
-      </div>
-      <div className="flex-[1] flex justify-center h-full">
-        {state.matches('editing') ? (
-          // <EditingPanel shape={selectedShape} send={send} />
-          <></>
-        ) : (
-          <div className="w-5/6 sm:w-1/2 h-full">
-            <Editor
-              initialDoc={code}
-              onChange={(val: string) =>
-                send({ type: 'UPDATE_CODE', code: val })
-              }
-            />
-          </div>
-        )}
-      </div>
+    <div className="h-dvh bg-black flex flex-col">
+      <SceneRenderer
+        shapes={shapes}
+        send={send}
+        selectedShape={selectedShape}
+        editorVisible={!state.matches('editing')}
+        code={code}
+        onCodeChange={(val: string) => send({ type: 'UPDATE_CODE', code: val })}
+      />
     </div>
   );
 };
--- src/components/SceneRenderer.tsx
+++ src/components/SceneRenderer.tsx
@@ -4,17 +4,24 @@ import { Physics } from '@react-three/rapier';
 import ShapeMesh from './ShapeMesh';
 import type { Shape } from '../types';
 import EditingPanel from './EditingPanel';
+import Editor from './Editor';
 
 interface SceneRendererProps {
   shapes: Shape[];
   send: (event: any) => void;
   selectedShape: Shape | undefined;
+  editorVisible: boolean;
+  code: string;
+  onCodeChange: (value: string) => void;
 }
 
 const SceneRenderer: React.FC<SceneRendererProps> = ({
   shapes,
   send,
   selectedShape,
+  editorVisible,
+  code,
+  onCodeChange,
 }) => {
   return (
     <Canvas camera={{ position: [0, 0, 5], fov: 75 }}>
@@ -40,16 +47,15 @@ const SceneRenderer: React.FC<SceneRendererProps> = ({
       </Physics>
       {selectedShape && (
         <Html fullscreen>
-          <div
-            style={{
-              position: 'absolute',
-              top: '1rem',
-              right: '1rem',
-              width: '300px',
-              zIndex: 10,
-            }}
-          >
+          <div className="absolute top-4 right-4 w-[300px]">
             <EditingPanel shape={selectedShape} send={send} />
+          </div>
+        </Html>
+      )}
+      {editorVisible && (
+        <Html fullscreen>
+          <div className="absolute bottom-4 left-1/2 transform -translate-x-1/2 w-5/6 sm:w-1/2 h-48">
+            <Editor initialDoc={code} onChange={onCodeChange} />
           </div>
         </Html>
       )}
